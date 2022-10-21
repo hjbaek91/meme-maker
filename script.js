4ba53417cddeb5abe3bd1bb5f40dcf89c5ec8ad8
HyunJun Baek <69138182+hjbaek91@users.noreply.github.com>
2022-10-21 15:49:19 +0900

    Applying paths on each content

--- script.js
+++ script.js
@@ -4,4 +4,13 @@ const ctx = canvas.getContext("2d"); // the "paint-brush" of the canvas
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(50, 50, 100, 200); // creating a rectangle
+ctx.rect(50, 50, 100, 100); // creating a rectangle
+ctx.rect(150, 150, 100, 100);
+ctx.rect(250, 250, 100, 100);
+ctx.fill();
+
+ctx.beginPath(); // creating a new path
+ctx.rect(350, 350, 100, 100);
+ctx.rect(450, 450, 100, 100);
+ctx.fillStyle = "red";
+ctx.fill();
